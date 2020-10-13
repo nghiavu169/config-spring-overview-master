efbe5723db20aae87575fbfbd1f2d3d56a9d0843
nghiavu169 <67424154+nghiavu169@users.noreply.github.com>
2020-10-13 09:27:00 +0700

    Add files via upload

---
+++ config-spring-overview-master/src/main/java/com/codegym/config/AppConfiguration.java
@@ -0,0 +1,30 @@
+package com.codegym.config;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.*;
+import org.springframework.context.annotation.*;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.codegym.controller")
+public class AppConfiguration extends WebMvcConfigurerAdapter implements ApplicationContextAware {
+
+    private ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    @Bean
+    public ViewResolver internalResourceViewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setSuffix(".jsp");
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setApplicationContext(applicationContext);
+        return viewResolver;
+    }
+}
